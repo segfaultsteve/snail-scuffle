7621267522561b4171adaafc3674ff1f38f14fc5
segfaultsteve <segfaultsteve@gmail.com>
2018-03-29 23:43:13 -0400

    added validation

--- common/src/main/java/com/snailscuffle/common/battle/BattleResult.java
+++ common/src/main/java/com/snailscuffle/common/battle/BattleResult.java
@@ -7,4 +7,11 @@ public class BattleResult implements Serializable {
 	
 	public List<BattleEvent> sequenceOfEvents;
 	
+	public void validate() {
+		if (sequenceOfEvents == null || sequenceOfEvents.size() == 0) {
+			throw new InvalidBattleException("Battle result not found");
+		}
+		sequenceOfEvents.forEach(e -> e.validate());
+	}
+	
 }
