103125b39b010e95da97f16454f6c17e3702a46e
segfaultsteve <segfaultsteve@gmail.com>
2018-04-02 21:27:06 -0400

    added winnerIndex for easy reference

--- common/src/main/java/com/snailscuffle/common/battle/BattleResult.java
+++ common/src/main/java/com/snailscuffle/common/battle/BattleResult.java
@@ -6,12 +6,17 @@ import java.util.List;
 public class BattleResult implements Serializable {
 	
 	public List<BattleEvent> sequenceOfEvents;
+	public int winnerIndex;		// 0 or 1; see BattleEvent for convention
 	
 	public void validate() {
 		if (sequenceOfEvents == null || sequenceOfEvents.size() == 0) {
 			throw new InvalidBattleException("Battle result not found");
 		}
 		sequenceOfEvents.forEach(e -> e.validate());
+		
+		if (winnerIndex < 0 || winnerIndex > 1) {
+			throw new InvalidBattleException("Invalid winner index; must be 0 (player 1) or 1 (player 2)");
+		}
 	}
 	
 }
