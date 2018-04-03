3dd2551e23d76142d64e81aec771cc2466177dd4
segfaultsteve <segfaultsteve@gmail.com>
2018-04-02 22:07:56 -0400

    removed rng seeds

--- common/src/main/java/com/snailscuffle/common/battle/BattleConfig.java
+++ common/src/main/java/com/snailscuffle/common/battle/BattleConfig.java
@@ -7,27 +7,22 @@ import java.util.List;
 public class BattleConfig implements Serializable {
 	
 	public List<BattlePlan> battlePlans;
-	public long firstHalfRngSeed;
-	public long secondHalfRngSeed;
 	
 	@SuppressWarnings("unused")
 	private BattleConfig() {}		// needed for serialization via jackson-jr
 	
-	public BattleConfig(BattlePlan p1FirstHalf, BattlePlan p2FirstHalf, long seed) {
+	public BattleConfig(BattlePlan p1FirstHalf, BattlePlan p2FirstHalf) {
 		battlePlans = new ArrayList<>();
 		battlePlans.add(p1FirstHalf);
 		battlePlans.add(p2FirstHalf);
-		firstHalfRngSeed = seed;
 	}
 	
-	public BattleConfig(BattlePlan p1FirstHalf, BattlePlan p2FirstHalf, BattlePlan p1SecondHalf, BattlePlan p2SecondHalf, long seed1, long seed2) {
+	public BattleConfig(BattlePlan p1FirstHalf, BattlePlan p2FirstHalf, BattlePlan p1SecondHalf, BattlePlan p2SecondHalf) {
 		battlePlans = new ArrayList<>();
 		battlePlans.add(p1FirstHalf);
 		battlePlans.add(p2FirstHalf);
 		battlePlans.add(p1SecondHalf);
 		battlePlans.add(p2SecondHalf);
-		firstHalfRngSeed = seed1;
-		secondHalfRngSeed = seed2;
 	}
 	
 	public void validate() throws InvalidBattleException {
