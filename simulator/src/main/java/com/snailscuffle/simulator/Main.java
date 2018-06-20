97098b5b317df82f873ae5e58975ca60fba4085f
alexperry0 <perry.alex93@gmail.com>
2018-06-19 22:39:06 -0400

    big changes: added ability to run simulations either locally or over the network; added ability to parse meta data from battle results; added ability to store battle results in SQLite database

--- simulator/src/main/java/com/snailscuffle/simulator/Main.java
+++ simulator/src/main/java/com/snailscuffle/simulator/Main.java
@@ -1,6 +1,7 @@
 package com.snailscuffle.simulator;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -13,11 +14,13 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import com.snailscuffle.common.battle.*;
 import com.snailscuffle.common.util.JsonUtil;
+import com.snailscuffle.game.battle.*;
 
 class Main {
 
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 	private static final String urlToConnectTo = "http://localhost:8080/battle";
+	private static SimulatorSettings simulatorSettings = new SimulatorSettings();
 
 	public static void main(String[] args) {
 		
@@ -29,24 +32,69 @@ class Main {
 	}
 	
 	public static void submitBattlePlansToGameServer(List<BattlePlan> generatedBattlePlans, PostAndResponseStructure httpClient) {
+
+		DatabaseUtil database = new DatabaseUtil();
+		List<BattlePlan> battlePlans = new ArrayList<BattlePlan>();
+		List<BattleResult> battleResults = new ArrayList<BattleResult>();
+		String response;
+		String jsonToSend;
+		BattleResult result = null;
+		int matchesProcessedCounter = 0;
+		int totalMatchesProcessed = 0;
+		int battleId = 0;
 		
-		Collections.shuffle(generatedBattlePlans); // Shuffle to get more diverse match-ups
-		
-		try {
-			for(int i = 0; i < generatedBattlePlans.size(); i+=2)
-			{
-				String response;
-				try {
-					String jsonToSend = JsonUtil.serialize(new BattleConfig(generatedBattlePlans.get(i), generatedBattlePlans.get(i+1),
-																			generatedBattlePlans.get(i), generatedBattlePlans.get(i+1) ));
-					response = httpClient.postMessage(urlToConnectTo, jsonToSend);
-					logger.debug(response);
+		for(int i = 0; i < generatedBattlePlans.size(); i++)
+		{
+			for(int j = 0; j < generatedBattlePlans.size();j++) {				
+				try {						
+						//Build a list that contains enough battle plans to ensure a battle always ends
+						battlePlans.addAll(Arrays.asList(generatedBattlePlans.get(i), generatedBattlePlans.get(j),
+								generatedBattlePlans.get(i), generatedBattlePlans.get(j),
+								generatedBattlePlans.get(i), generatedBattlePlans.get(j),
+								generatedBattlePlans.get(i), generatedBattlePlans.get(j),
+								generatedBattlePlans.get(i), generatedBattlePlans.get(j),
+								generatedBattlePlans.get(i), generatedBattlePlans.get(j)));
+						
+						int winnerFound = -1;
+						while(winnerFound < 0) {
+							if (simulatorSettings.goOverNetwork) {									
+								BattleConfig battleConfig = new BattleConfig(battlePlans.toArray(new BattlePlan[0]));
+								jsonToSend = JsonUtil.serialize(battleConfig);
+								response = httpClient.postMessage(urlToConnectTo, jsonToSend);
+								result = JsonUtil.deserialize(BattleResult.class, response);									
+								battleResults.add(result);
+								winnerFound = result.winnerIndex;
+								battlePlans.addAll(Arrays.asList(generatedBattlePlans.get(i), generatedBattlePlans.get(j)));
+							} else {									
+								//For now just keep sending the same one																
+								BattleConfig battleConfig = new BattleConfig(battlePlans.toArray(new BattlePlan[0]));
+								result = (new Battle(battleConfig)).getResult();
+								battleResults.add(result);
+								winnerFound = result.winnerIndex;
+								battlePlans.addAll(Arrays.asList(generatedBattlePlans.get(i), generatedBattlePlans.get(j)));
+							}							
+						}
+
+					BattleResultMetaData metaData = new BattleResultMetaData(battleResults);
+					database.StoreResult(Arrays.asList(generatedBattlePlans.get(i), generatedBattlePlans.get(j)), metaData, battleId);
+
+					totalMatchesProcessed++;
+					matchesProcessedCounter++;
+					battleId++;
+					battlePlans.clear();
+					battleResults.clear();
+					
+					if (matchesProcessedCounter >= 1000000) {
+						logger.debug("Processed " + totalMatchesProcessed + "/" + (generatedBattlePlans.size() * generatedBattlePlans.size()) + " matches.");
+						matchesProcessedCounter = 0;
+					}
+					
 				} catch (Exception e) {
-					logger.error("Error when trying to POST battle plans. Error message: " + e.getMessage());
-				}
-			}
-		} catch (ArrayIndexOutOfBoundsException e){
-			//log some kind of message
+					logger.error("Error when trying to simulate battle. Error message: " + e.getMessage());
+					for(BattlePlan battlePlan : battlePlans)
+						logger.debug(battlePlan.toString());			
+				}							
+			}		
 		}
 	}
 	
