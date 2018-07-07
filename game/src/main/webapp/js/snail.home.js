d0d8f69f33fe3475c98838440a0babd3428a70cc
segfaultsteve <segfaultsteve@gmail.com>
2018-07-06 23:51:26 -0400

    added home page

---
+++ game/src/main/webapp/js/snail.home.js
@@ -0,0 +1,10 @@
+var snail = (function (snail) {
+	snail.home = snail.home || {};
+	snail.model = snail.model || {};
+	
+	snail.home.init = function ($container) {
+		
+	};
+	
+	return snail;
+}(snail || {}));
--- game/src/main/webapp/js/snail.js
+++ game/src/main/webapp/js/snail.js
@@ -1,16 +1,23 @@
 var snail = (function(snail, $) {
 	// private variables
 	const pages = {
+		$home: $('#home'),
 		$battleplan: $('#battleplan')
 	};
 	
 	// public methods
 	snail.init = function () {
+		for (let key in pages) {
+			pages[key].hide();
+		}
+		
 		snail.model.init();
 		snail.model.battleplan.init(snail.data);
 		snail.model.battle.init();
+		snail.home.init();
 		snail.battleplan.init(pages.$battleplan);
-		pages.$battleplan.show();
+		
+		pages.$home.show();
 	};
 	
 	return snail;
